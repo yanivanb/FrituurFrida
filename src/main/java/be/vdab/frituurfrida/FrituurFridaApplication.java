43798ae7cabd48ad77e0218de34a4c1dfe76ecce
Lightflamingo <Lightflamingowilreborn@gmail.com>
2022-07-04 11:28:37 +0200

    Project & Welkom Pagina

---
+++ src/main/java/be/vdab/frituurfrida/FrituurFridaApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.frituurfrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrituurFridaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrituurFridaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/frituurfrida/FrituurFridaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.frituurfrida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrituurFridaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
