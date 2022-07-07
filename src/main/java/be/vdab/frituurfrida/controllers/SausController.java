f55ed49a142613e0aa73a0c19eeb1b66d6703957
yaniv <yani.vanboxelaere@vdabcampus.be>
2022-07-07 10:45:30 +0200

    Path Variable

--- src/main/java/be/vdab/frituurfrida/controllers/SausController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SausController.java
@@ -3,7 +3,11 @@ package be.vdab.frituurfrida.controllers;
 import be.vdab.frituurfrida.domain.Saus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Controller
 class SausController {
@@ -16,5 +20,16 @@ class SausController {
     @GetMapping("/sauzen")
     public ModelAndView findAll() {
         return new ModelAndView("sauzen", "alleSauzen", alleSauzen);
+    }
+
+    private Optional<Saus> findByIdHelper(long id) {
+        return Arrays.stream(alleSauzen).filter(saus->saus.getId()==id).findFirst();
+    }
+    @GetMapping("/sauzen/{id}")
+    public ModelAndView findById(@PathVariable long id) {
+        var modelAndView = new ModelAndView("saus");
+        findByIdHelper(id).ifPresent(gevondenSaus ->
+                modelAndView.addObject("saus", gevondenSaus));
+        return modelAndView;
     }
 }
